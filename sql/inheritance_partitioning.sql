5d1b93dba46b1f8f3247f345c329b4104bc756fb
Mathis Van Eetvelde <mathis.vaneetvelde@protonmail.com>
2022-09-15 12:52:14 -0600

    Change from trades to measurement

--- sql/inheritance_partitioning.sql
+++ sql/inheritance_partitioning.sql
@@ -43,27 +43,27 @@ CREATE TRIGGER insert_measurement_trigger
     BEFORE INSERT ON measurement
     FOR EACH ROW EXECUTE FUNCTION measurement_insert_trigger();
    
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-01-3', 2, 2.9);
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-01-6', 1, 3.5);
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-01-15', 3, 6.2);
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-01-23', 1, 3.2);
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-01-28', 2, 5.2);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-01-3', 2, 2.9);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-01-6', 1, 3.5);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-01-15', 3, 6.2);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-01-23', 1, 3.2);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-01-28', 2, 5.2);
 
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-02-04', 3, 4.1);
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-02-09', 1, 5.2);
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-02-13', 2, 2.6);
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-02-24', 2, 1.7);
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-02-28', 1, 6.1);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-02-04', 3, 4.1);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-02-09', 1, 5.2);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-02-13', 2, 2.6);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-02-24', 2, 1.7);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-02-28', 1, 6.1);
 
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-03-6', 3, 5.2);
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-03-8', 2, 2.3);
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-03-12', 1, 1.6);
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-03-27', 3, 2.5);
-INSERT INTO measurement (time, symbolID, price) VALUES ('2022-03-29', 2, 5.3);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-03-6', 3, 5.2);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-03-8', 2, 2.3);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-03-12', 1, 1.6);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-03-27', 3, 2.5);
+INSERT INTO measurement (time, sensorID, sensor_value) VALUES ('2022-03-29', 2, 5.3);
 
 SELECT tableoid, * from measurement;
 
--- tableoid |          time          | price | symbolid
+-- tableoid |          time          | sensor_value | sensorID
 ------------+------------------------+-------+----------
 --    19103 | 2022-01-03 07:00:00+00 |   2.9 |        2
 --    19103 | 2022-01-06 07:00:00+00 |   3.5 |        1
@@ -95,14 +95,6 @@ WHERE
 -- measurement_y2022m02 | measurement_y2022m02_time_idx
 -- measurement_y2022m03 | measurement_y2022m03_time_idx
 --(3 rows)
-
-tableoid |          time          | price | symbolid
-----------+------------------------+-------+----------
-   19103 | 2022-01-03 07:00:00+00 |   2.9 |        2
-   19103 | 2022-01-06 07:00:00+00 |   3.5 |        1
-     …   |           …            |     … |        … 
-   19109 | 2022-03-27 06:00:00+00 |   2.5 |        3
-   19109 | 2022-03-29 06:00:00+00 |   5.3 |        2
 
 
 SET enable_partition_pruning = on;
